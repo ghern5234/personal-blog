1fe836351332a811297367d00a9d39e3dc52f554
George <ghern5234@gmail.com>
2024-03-19 22:18:31 -0500

    made additions to css and js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,2 +1,17 @@
-const blogInfo = localStorage.getItem("formContent");
+const blogContainer = document.getElementById("container");
+const blogInfo = JSON.parse(localStorage.getItem("formContent"));
 console.log("formContent", blogInfo);
+
+for (const blog of blogInfo) {
+    const blogElement = document.createElement("div");
+    const blogTitle = document.createElement("h2");
+    const blogContent = document.createElement("p");
+
+    blogTitle.textContent = blog.title;
+    blogContent.textContent = blog.content;
+    
+    blogElement.appendChild(blogTitle);
+    blogElement.appendChild(blogContent);
+    blogContainer.appendChild(blogElement);
+}
+
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,53 @@
-// themeButtonEl.on('click', function () {
-//     if (isDark) {
-//       $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
-//       isDark = !isDark;
+
+
+// const button = document.getElementById("submit");
+// let isDark = false;
+
+// function toggle() {
+//     if(isDark) {
+//         lightMode();
 //     } else {
-//       $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
-//       isDark = !isDark;
+//         darkMode();
 //     }
-//   });
-  
+// };
+
+// const lightMode = function () {
+//     const elements = document.querySelectorAll(".dark");
+    
+// };
+
+
+// button.addEventListener("click")
+
+
+const button = document.getElementById("submit");
+
+let isDark = false;
+
+function toggle() {
+  if (isDark) {
+    lightMode();
+  } else {
+    darkMode();
+  }
+  isDark = !isDark;
+}
+
+const lightMode = function () {
+  const elements = document.querySelectorAll(".dark");
+  console.log(elements);
+  for (const element of elements) {
+    element.classList.add("light");
+    element.classList.remove("dark");
+  }
+};
+
+const darkMode = function () {
+  const elements = document.querySelectorAll(".light");
+  for (const element of elements) {
+    element.classList.add("dark");
+    element.classList.remove("light");
+  }
+};
+
+button.addEventListener("click", toggle);
