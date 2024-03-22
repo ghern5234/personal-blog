fb63ae95a1ab054c0f47ce4237f57218d5c603de
George <ghern5234@gmail.com>
2024-03-22 00:57:44 -0500

    Added footer to blog list page, added google fonts to blogl.html, finished notation and styling for all files

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,26 +1,44 @@
+//Creating global variable for blog container via container div
 const blogContainer = document.getElementById("container");
+//Calling form content from local storage and parsing froming JSON string.
+//Storing that data into blog variable
 const blogInfo = JSON.parse(localStorage.getItem("formContent"));
 
-
+//For loop to create blog post from info
 for (const blog of blogInfo) {
+
+ //Creating elements and storing them in variables
     const blogElement = document.createElement("div");
     const blogTitle = document.createElement("h2");
-    const blogContent = document.createElement("p");
+    const blogContent = document.createElement("h3");
+    const blogAuthor = document.createElement("p");
 
+//Taking specific and individual text info and storing it in the corresponding
+//elements/variables just created
     blogTitle.textContent = blog.title;
     blogContent.textContent = blog.content;
-    
+    blogAuthor.textContent = "Posted by: " + blog.usernameInput;
+
+//Attaching the variable/elements to the page   
     blogElement.appendChild(blogTitle);
     blogElement.appendChild(blogContent);
+    blogElement.appendChild(blogAuthor);
     blogContainer.appendChild(blogElement);
 }
 
+//Calling the back but element and storing it in a global variable 
 const backButton = document.getElementById("btn");
 
+//Function for the back button
 function returnAnchor(event) {
     event.preventDefault();
+
+//Call the back button element and store in variable
     const returnBtn = document.createElement("button")
+
+//Link form page to be ran
     window.location.href="./index.html";
 };
 
+//Attach on event listener to intialize the back button function when clicked
 backButton.addEventListener("click", returnAnchor)
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,37 +1,18 @@
-// const button = document.getElementById("submit");
-// let isDark = false;
-
-// function toggle() {
-//     if(isDark) {
-//         lightMode();
-//     } else {
-//         darkMode();
-//     }
-// };
-
-// const lightMode = function () {
-//     const elements = document.querySelectorAll(".dark");
-    
-// };
-
-
-// button.addEventListener("click")
-
-
+//Creating a global variable containing the tobble button
 const button = document.getElementById("toggle");
 
+//Defining 
 let isDark = false;
 
-
+//Function for setting light mode from dark mode
 const lightMode = function () {
   const elements = document.querySelectorAll(".dark");
-  // console.log(elements);
   for (const element of elements) {
     element.classList.add("light");
     element.classList.remove("dark");
   }
 };
-
+//Function for settiing dark mode from light mode
 const darkMode = function () {
   const elements = document.querySelectorAll(".light");
   for (const element of elements) {
@@ -40,6 +21,8 @@ const darkMode = function () {
   }
 };
 
+//Toggle function containing an statement statement for turning dark mode and vice verse, 
+//as well as switching the variable rom false to true and vice versa
 function toggle() {
   if (isDark) {
     lightMode();
@@ -49,4 +32,5 @@ function toggle() {
   isDark = !isDark;
 }
 
+//Setting an on click event listener to initiate toggle function
 button.addEventListener("click", toggle);
